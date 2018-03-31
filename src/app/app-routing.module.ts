505d5085bf3baf908d7f64dfcaf625f3bd8207f5
Michael Harris <michaeljh7@gmail.com>
2018-03-31 14:03:54 -0600

    Added service list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { ServiceListComponent } from './service/service-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent }
+  { path: 'services', component: ServiceListComponent }
 ]
 
 @NgModule({
