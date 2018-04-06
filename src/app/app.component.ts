d8e6c504f61a907d769720c9db38356a0fe2022c
Michael Harris <michaeljh7@gmail.com>
2018-04-06 06:35:29 -0600

    Updated app component to include function to scroll to top of screen when router link loads a new path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   today: Date = new Date();
 
-  public href: string = "";
+  public currentPath: string = "";
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.router.events.subscribe((res) => {
-      this.href = this.router.url;
-    })
+      this.currentPath = this.router.url;
+    });
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+            return;
+      }
+      window.scrollTo(0, 0)
+    });
   }
 }
