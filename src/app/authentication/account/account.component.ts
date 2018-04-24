b3958ef45d094f10c3b4288fd3c0ac2627008524
Michael Harris <michaeljh7@gmail.com>
2018-04-24 04:10:09 -0600

    Implemented multiple forms on account page

--- src/app/authentication/account/account.component.ts
+++ src/app/authentication/account/account.component.ts
@@ -17,15 +17,13 @@ export class AccountComponent {
   submitted: boolean = false;
   failedAccount: boolean = false;
   accountReset: boolean = false;
-  accountForm: FormGroup;
-  accountFormNoPassword: FormGroup;
-  accountFormPassword: FormGroup;
   accountFormBasic: FormGroup;
+  accountFormPassword: FormGroup;
+  // accountForm: FormGroup;
   resetString: string;
   error: string;
   formData: string = '';
-  formBasicSet: boolean = false;
-  formArray: FormArray;
+  formBasicSet: boolean = true;
 
   constructor(
     private tokenService: Angular2TokenService,
@@ -36,12 +34,10 @@ export class AccountComponent {
       this.userData = this.tokenService.currentUserData;
       this.authData = this.tokenService.currentAuthData;
 
-      this.accountFormNoPassword = fb.group({
+      this.accountFormBasic = fb.group({
         name: [this.userData.name],
         email: [this.userData.email, [ValidationService.emailRequired, ValidationService.emailValidator]],
-        passwordCurrent: ['', [ValidationService.passwordRequired, ValidationService.passwordValidator]],
-        password: '',
-        passwordConfirmation: ''
+        passwordCurrent: ['', [ValidationService.passwordRequired, ValidationService.passwordValidator]]
       }, {});
 
       this.accountFormPassword = fb.group({
@@ -53,43 +49,38 @@ export class AccountComponent {
       }, {
         validator: ValidationService.passwordMatch
       });
-
-      this.accountFormBasic = fb.group({
-        name: [this.userData.name],
-        email: [this.userData.email, [ValidationService.emailRequired, ValidationService.emailValidator]],
-        passwordCurrent: ['', [ValidationService.passwordRequired, ValidationService.passwordValidator]]
-      }, {});
-
-      this.accountForm = this.accountFormNoPassword;
   }
 
   submit(value: any) {
     this.submitted = true;
-    if(this.accountForm.controls.password.value === '' && this.accountForm.controls.passwordConfirmation.value === '') {
-      this.accountFormBasic.controls.name = this.accountForm.controls.name;
-      this.accountFormBasic.controls.email = this.accountForm.controls.email;
-      this.accountFormBasic.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
-      setTimeout(() => {
-        this.formBasicSet = true;
-        console.log('success 1');
-        if(this.formBasicSet == true) {
-          setTimeout(() => {
-            this.accountForm = this.accountFormBasic;
-            // this.accountForm.controls.removeAt(4);
-            console.log('success 2');
-            if(this.accountForm == this.accountFormBasic) {
-              setTimeout(() => {
-                this.saveUserData(value);
-                console.log('success 3');
-              }, 500);
-            }
-          }, 500);
-        }
-      }, 500);
-    } else {
+    // if(this.accountForm.controls.password.value === '' && this.accountForm.controls.passwordConfirmation.value === '') {
+    //   this.accountFormBasic.controls.name = this.accountForm.controls.name;
+    //   this.accountFormBasic.controls.email = this.accountForm.controls.email;
+    //   this.accountFormBasic.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
+    //   setTimeout(() => {
+    //     this.formBasicSet = true;
+    //     console.log('success 1');
+    //     if(this.formBasicSet == true) {
+    //       setTimeout(() => {
+    //         this.accountForm = this.accountFormBasic;
+    //         // this.accountForm.controls.password;
+    //         // this.accountForm.controls.passwordConfirmation;
+    //         this.accountForm.removeControl("password");
+    //         this.accountForm.removeControl("passwordConfirmation");
+    //         console.log('success 2');
+    //         if(this.accountForm == this.accountFormBasic && !this.accountForm.contains('passwordConfirmation')) {
+    //           setTimeout(() => {
+    //             this.saveUserData(value);
+    //             console.log('success 3');
+    //           }, 500);
+    //         }
+    //       }, 500);
+    //     }
+    //   }, 500);
+    // } else {
       this.saveUserData(value);
       console.log('success 4');
-    }
+    // }
 
 
     // this.authService.resetPassword(value).subscribe(
@@ -102,37 +93,37 @@ export class AccountComponent {
   //   this.userData = name
   // }
 
-formChange(value: any) {
-  if(this.accountForm !== this.accountFormPassword) {
-    this.accountFormPassword.controls.name = this.accountForm.controls.name;
-    this.accountFormPassword.controls.email = this.accountForm.controls.email;
-    this.accountFormPassword.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
-    this.accountFormPassword.controls.password = this.accountForm.controls.password;
-    this.accountFormPassword.controls.passwordConfirmation = this.accountForm.controls.passwordConfirmation;
-    setTimeout(() => {
-      this.accountForm = this.accountFormPassword;
-      setTimeout(() => {
-        this.accountForm.updateValueAndValidity();
-        this.formReset = true;
-        console.log(1);
-        // this.accountForm.markAsUntouched();
-      });
-    });
-  } else if(value.controls.password.value == '') {
-    setTimeout(() => {
-      this.accountFormNoPassword.controls.name = this.accountForm.controls.name;
-      this.accountFormNoPassword.controls.email = this.accountForm.controls.email;
-      this.accountFormNoPassword.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
-      this.accountFormNoPassword.controls.password = this.accountForm.controls.password;
-      this.accountFormNoPassword.controls.passwordConfirmation = this.accountForm.controls.passwordConfirmation;
-      this.accountForm = this.accountFormNoPassword;
-      this.accountForm.updateValueAndValidity();
-      this.formReset = true;
-      console.log(2);
-      // this.accountForm.markAsUntouched();
-    });
-  }
-}
+// formChange(value: any) {
+//   if(this.accountForm !== this.accountFormPassword) {
+//     this.accountFormPassword.controls.name = this.accountForm.controls.name;
+//     this.accountFormPassword.controls.email = this.accountForm.controls.email;
+//     this.accountFormPassword.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
+//     this.accountFormPassword.controls.password = this.accountForm.controls.password;
+//     this.accountFormPassword.controls.passwordConfirmation = this.accountForm.controls.passwordConfirmation;
+//     setTimeout(() => {
+//       this.accountForm = this.accountFormPassword;
+//       setTimeout(() => {
+//         this.accountForm.updateValueAndValidity();
+//         this.formReset = true;
+//         console.log(1);
+//         // this.accountForm.markAsUntouched();
+//       });
+//     });
+//   } else if(value.controls.password.value == '') {
+//     setTimeout(() => {
+//       this.accountFormNoPassword.controls.name = this.accountForm.controls.name;
+//       this.accountFormNoPassword.controls.email = this.accountForm.controls.email;
+//       this.accountFormNoPassword.controls.passwordCurrent = this.accountForm.controls.passwordCurrent;
+//       this.accountFormNoPassword.controls.password = this.accountForm.controls.password;
+//       this.accountFormNoPassword.controls.passwordConfirmation = this.accountForm.controls.passwordConfirmation;
+//       this.accountForm = this.accountFormNoPassword;
+//       this.accountForm.updateValueAndValidity();
+//       this.formReset = true;
+//       console.log(2);
+//       // this.accountForm.markAsUntouched();
+//     });
+//   }
+// }
 
 formChangeRefresh(value: any) {
   setTimeout(() => {
@@ -169,7 +160,6 @@ saveUserData(value: any) {
       },
       error => {
         console.log('Error saving user ' + error);
-        this.error = error;
       }
     );
   // this.authService.updatePassword(value.password, value.passwordCurrent).subscribe(
@@ -183,20 +173,37 @@ saveUserData(value: any) {
 
     this.failedAccount = true;
     this.submitted = false;
-    this.accountForm.markAsUntouched();
 
-    for(let attribute in this.accountForm.controls) {
-      if (parsed_errors[attribute]) {
-        this.accountForm.controls[attribute]
-            .setErrors(parsed_errors[attribute]);
+    if (this.formBasicSet) {
+      this.accountFormBasic.markAsUntouched();
+
+      for(let attribute in this.accountFormBasic.controls) {
+        if (parsed_errors[attribute]) {
+          this.accountFormBasic.controls[attribute]
+              .setErrors(parsed_errors[attribute]);
+        }
       }
+      this.accountFormBasic.setErrors(parsed_errors);
+    } else {
+      this.accountFormPassword.markAsUntouched();
+
+      for(let attribute in this.accountFormPassword.controls) {
+        if (parsed_errors[attribute]) {
+          this.accountFormPassword.controls[attribute]
+              .setErrors(parsed_errors[attribute]);
+        }
+      }
+      this.accountFormPassword.setErrors(parsed_errors);
     }
-    this.accountForm.setErrors(parsed_errors);
   }
 
   resetTouch() {
     setTimeout(() => {
-      this.accountForm.markAsUntouched();
+      if (this.formBasicSet) {
+        this.accountFormBasic.markAsUntouched();
+      } else {
+        this.accountFormPassword.markAsUntouched();
+      }
       this.accountReset = true;
     });
   }
