eeccbee95ec8f8c85043d60dd5bbaa4eacce5cf2
Michael Harris <michaeljh7@gmail.com>
2018-05-02 11:45:11 -0600

    Fixed form submission message for admin login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -46,6 +46,8 @@ export class LoginComponent {
           setTimeout(() => {
             this.authService.redirectAfterLogin();
           },1000);
+
+          this.submitted = true;
         },
         err => this.afterFailedAdminLogin(err)
       );
