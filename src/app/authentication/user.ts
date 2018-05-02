d296b1570fb25aeec440abcfb4a916e1f2d47afe
Michael Harris <michaeljh7@gmail.com>
2018-05-02 11:02:12 -0600

    Got rid of unneeded user model

--- src/app/authentication/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  constructor(
-    public id: number = 0,
-    public name: string = '',
-    public email: string = '',
-    // public password: string = '',
-    // public passwordConfirmation: string = '',
-  ) {}
-}
