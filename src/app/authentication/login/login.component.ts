dd674c08cffb2b967dd0289ac6f1b067aa734f7f
Michael Harris <michaeljh7@gmail.com>
2018-05-04 11:13:19 -0600

    Added comment-user loading on login component

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ValidationService } from '../validation.service';
 import { AuthenticationService } from '../authentication.service';
+import { ArticleService } from '../../news/article.service';
+import { Angular2TokenService, UserData } from 'angular2-token';
+import { CommentUser } from '../../news/comment-user';
 
 
 @Component({
@@ -9,11 +13,17 @@ import { AuthenticationService } from '../authentication.service';
 })
 
 export class LoginComponent {
+  userData: UserData = <UserData>{};
+  userType: string;
+  commentUser: CommentUser = <CommentUser>{};
+  commentUsers: CommentUser[];
   submitted: boolean = false;
   loginForm: FormGroup;
   resetString: string;
 
   constructor(
+    private articleService: ArticleService,
+    private tokenService: Angular2TokenService,
     private authService: AuthenticationService,
     private formBuilder: FormBuilder,
     fb: FormBuilder
@@ -27,32 +37,34 @@ export class LoginComponent {
       })
   }
 
+
+  //Login functions
   submit(value: any) {
-    this.authService.logIn(value.email, value.password).subscribe(
-        res => {
-          setTimeout(() => {
-            this.authService.redirectAfterLogin();
-          },1000);
+    this.authService.logIn(value.email, value.password)
+    .subscribe(
+      res => {
+        this.userData = this.tokenService.currentUserData;
+        this.userType = this.tokenService.currentUserType;
+        this.getCommentUsers();
 
-          this.submitted = true;
-        },
-        err => this.afterFailedUserLogin(value)
-      );
+        this.submitted = true;
+      },
+      err => this.afterFailedUserLogin(value)
+    );
   }
-
   afterFailedUserLogin(value: any) {
-    this.authService.logInAdmin(value.email, value.password).subscribe(
-        res => {
-          setTimeout(() => {
-            this.authService.redirectAfterLogin();
-          },1000);
+    this.authService.logInAdmin(value.email, value.password)
+    .subscribe(
+      res => {
+        this.userData = this.tokenService.currentUserData;
+        this.userType = this.tokenService.currentUserType;
+        this.getCommentUsers();
 
-          this.submitted = true;
-        },
-        err => this.afterFailedAdminLogin(err)
-      );
+        this.submitted = true;
+      },
+      err => this.afterFailedAdminLogin(err)
+    );
   }
-
   afterFailedAdminLogin(errors: any) {
     let parsed_errors = JSON.parse(errors._body).errors;
 
@@ -66,10 +78,59 @@ export class LoginComponent {
 
     this.submitted = false;
   }
-
   resetSubmit() {
     setTimeout(() => {
       this.resetString = "<p class='alert alert-success mt-4' role='alert'>User account logged in successfully. Redirecting to homepage.</p>";
     });
+  }
+
+
+  //CommentUser functions
+  createCommentUserIfNull() {
+    if(this.commentUsers) {
+      if(!this.searchCommentUser(this.userData.id, this.userType)) {
+        let commentUser:CommentUser;
+        this.commentUser.user_id = this.userData.id;
+        this.commentUser.user_type = this.userType;
+        this.commentUser.user_name = this.userData.name;
+        this.articleService.createCommentUser(this.commentUser).subscribe(
+          res => {
+            console.log('Comment User created successfully');
+            this.articleService.getCommentUsers().subscribe(
+              commentUsers => {
+                this.commentUsers = commentUsers;
+                for(let commentUser of this.commentUsers) {
+                  console.log(commentUser);
+                }
+              },
+              err => console.log(err)
+            );
+          },
+          err => console.log(err)
+          );
+      }
+      setTimeout(() => {
+        this.authService.redirectAfterLogin();
+      },1000);
+    }
+  }
+  searchCommentUser(user_id:number, user_type:string):boolean {
+    let match:boolean = false;
+    for(let commentUser of this.commentUsers) {
+      if((commentUser.user_id === user_id) && (commentUser.user_type === user_type))
+        return match = true;
+    }
+  }
+  getCommentUsers() {
+    this.articleService.getCommentUsers()
+    .subscribe(
+      commentUsers => {
+        this.commentUsers = commentUsers;
+
+        if(this.commentUsers)
+          this.createCommentUserIfNull();
+      },
+      err => console.log(err)
+    );
   }
 }
--- src/app/news/article.service.ts
+++ src/app/news/article.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Angular2TokenService, UserData } from 'angular2-token';
 import { Article } from './article';
+import { Comment } from './comment';
+import { CommentUser } from './comment-user';
 
 
 @Injectable()
@@ -10,41 +13,81 @@ export class ArticleService {
   headers: Headers;
   options: RequestOptions;
   private articlesUrl = 'http://localhost:3001/articles';
+  private commentsUrl = 'http://localhost:3001/comments';
+  private commentUsersUrl = 'http://localhost:3001/comment_users';
 
-  constructor(private http: Http) {
+  constructor(
+    private http: Http,
+  ) {
     this.headers = new Headers({ 'Content-Type': 'application/json' });
     this.options = new RequestOptions({ headers: this.headers });
   }
 
+  // Article functions
   getArticles(): Observable<Article[]> {
     return this.http.get(this.articlesUrl)
                     .map(this.extractData)
                     .catch(this.handleError);
   }
-
   getArticle(id: number) {
     return this.http.get(this.articlesUrl + "/" + id + ".json");
   }
-
   createArticle(article: Article): Observable<Article> {
     return this.http.post(this.articlesUrl, JSON.stringify(article),
       this.options).map(this.extractData)
           .catch(this.handleError);
   }
-
   deleteArticle(id: number): Observable<Article> {
     const url = `${this.articlesUrl}/${id}`;
     return this.http.delete(url, this.options)
       .map(this.extractData)
       .catch(this.handleError);
   }
-
   updateArticle(article: Article): Observable<Article> {
     const url = `${this.articlesUrl}/${article.id}`;
     return this.http.put(url, JSON.stringify(article),
       this.options).map((this.extractData))
           .catch(this.handleError);
   }
+
+
+  // CommentUser functions
+  getCommentUsers(): Observable<CommentUser[]> {
+    return this.http.get(this.commentUsersUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+  getCommentUser(id: number) {
+    return this.http.get(this.commentUsersUrl + "/" + id + ".json");
+  }
+  createCommentUser(commentUser:CommentUser): Observable<CommentUser> {
+    return this.http.post(this.commentUsersUrl, JSON.stringify(commentUser),
+      this.options).map(this.extractData)
+          .catch(this.handleError);
+  }
+  deleteCommentUser(id: number): Observable<CommentUser> {
+    const url = `${this.commentUsersUrl}/${id}`;
+    return this.http.delete(url, this.options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+  updateCommentUser(commentUser: CommentUser): Observable<CommentUser> {
+    const url = `${this.commentUsersUrl}/${commentUser.id}`;
+    return this.http.put(url, JSON.stringify(commentUser),
+      this.options).map((this.extractData))
+          .catch(this.handleError);
+  }
+
+
+  // Comment functions
+  createComment(commentUserId:number, articleId:number, comment:Comment): Observable<Comment> {
+    comment.comment_user_id = commentUserId;
+    comment.article_id = articleId;
+    return this.http.post(this.commentsUrl, JSON.stringify(comment),
+      this.options).map(this.extractData)
+          .catch(this.handleError);
+  }
+
 
   private extractData(res: Response) {
       let body = res.json();
