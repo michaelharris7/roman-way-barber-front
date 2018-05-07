7b132482613881cc27e8e793b464630f6e189097
Michael Harris <michaeljh7@gmail.com>
2018-05-07 17:00:48 -0600

    created redirection to kick non-admins out of article-new page

--- src/app/news/article-new.component.ts
+++ src/app/news/article-new.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Article } from './article';
 import { ArticleService } from './article.service';
+import { AuthenticationService } from '../authentication/authentication.service';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   selector: 'article-new',
@@ -9,13 +11,24 @@ import { ArticleService } from './article.service';
   styleUrls: ['article.component.css']
 })
 
-export class ArticleNewComponent {
+export class ArticleNewComponent implements OnInit {
   article = new Article;
   submitted: boolean = false;
+  userType: string;
 
   constructor(
     private articleService: ArticleService,
+    private tokenService: Angular2TokenService,
+    private authService: AuthenticationService
   ) {}
+
+  ngOnInit() {
+    if(this.isLoggedIn()) {
+      this.userType = this.tokenService.currentUserType;
+      console.log(this.userType);
+    }
+    this.redirectIfNotAdmin();
+  }
 
   createArticle(article) {
     this.submitted = true;
@@ -27,5 +40,13 @@ export class ArticleNewComponent {
             return Observable.throw(error);
           }
         );
+  }
+  redirectIfNotAdmin() {
+    if(this.userType !== 'ADMIN') {
+      this.authService.redirectAfterLogin();
+    }
+  }
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
