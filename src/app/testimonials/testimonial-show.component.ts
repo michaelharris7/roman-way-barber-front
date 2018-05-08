cbd807852a3478a0ce6af1a22b6f384d27f6850e
Michael Harris <michaeljh7@gmail.com>
2018-05-08 13:58:26 -0600

    Added content and styles to testimonial show page

--- src/app/testimonials/testimonial-show.component.ts
+++ src/app/testimonials/testimonial-show.component.ts
@@ -22,10 +22,58 @@ export class TestimonialShowComponent implements OnInit {
   testimonialUser: TestimonialUser = <TestimonialUser>{};
   testimonialUsers: TestimonialUser[];
   testimonials: Testimonial[];
-  testimonial: Testimonial = <Testimonial>{}
+  // testimonial: Testimonial = <Testimonial>{}
+
+  @Input() testimonial: Testimonial;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private tokenService: Angular2TokenService,
+    private authService: AuthenticationService,
+    private testimonialService: TestimonialService
+  ) {}
 
   ngOnInit() {
-
+    this.loginVerification();
+    this.getTestimonial();
   }
 
+
+  // Testimonial Function
+  getTestimonial() {
+    let testimonialRequest = this.route.params
+        .flatMap((params: Params) =>
+          this.testimonialService.getTestimonial(+params['id']));
+    testimonialRequest.subscribe(response => this.testimonial = response.json());
+  }
+
+
+  // Account Functions
+  loginVerification() {
+    if(this.isLoggedIn()) {
+      this.tokenService.validateToken().subscribe(
+        res => {
+          this.userData = this.tokenService.currentUserData;
+          this.userType = this.tokenService.currentUserType;
+        },
+        err => {
+          console.log('No user logged in. Logging out:' + err);
+          this.logOut();
+        }
+      );
+    }
+  }
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+  logOut() {
+    this.authService.logOut().subscribe(
+        res => { return true },
+        err => { console.log('There was an error in logging out:' + err); }
+      );
+  }
+  isAdmin(): boolean {
+    return (this.isLoggedIn() && this.userType === 'ADMIN');
+  }
 }
