eca31ee5d277525b0f3abb8bba6c7d286ecfbdcb
Michael Harris <michaeljh7@gmail.com>
2018-05-11 07:29:52 -0600

    Updated more files to be compatible with heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/src/app'));
 app.listen(process.env.PORT || 5000);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
