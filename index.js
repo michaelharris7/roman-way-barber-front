4eb30b60a70592f27eeac9268c52cc07363e3d36
Michael Harris <michaeljh7@gmail.com>
2018-05-11 10:34:02 -0600

    More heroku changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const app = express();
+const app = express()
 const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/src/app'));
+// app.use(express(path.join('/src/')));
+app.use(express.static(__dirname + '/src/'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'))
+});
 app.listen(process.env.PORT || 5000);
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
-})
 
 console.log('Console listening!');
 
@@ -16,10 +18,10 @@ console.log('Console listening!');
 // const PORT = process.env.PORT || 5000
 
 // express()
-//   .use(express.static(path.join(__dirname, 'src/app')))
+//   .use(express.static(__dirname + '/src/'))
 //   // .use(express.static(__dirname))
 
 //   .get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/src/index.html'));
+//     res.sendFile(path.join(__dirname + '/src/index.html'))
 //   })
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express(path.join(__dirname, '/src')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log('Your node js server is running');
 });
