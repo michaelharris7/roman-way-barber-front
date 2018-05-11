10581096721f3f0537620eacb1c0809ded8f8b6e
Michael Harris <michaeljh7@gmail.com>
2018-05-11 05:55:45 -0600

    Made changes to get app compatible with heroku

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use(express.static(__dirname + '/src'));
+app.listen(process.env.PORT || 5000);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+})
+
+console.log('Console listening!');
+
+
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'src/app')))
+//   // .use(express.static(__dirname))
+
+//   .get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/src/index.html'));
+//   })
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,43 @@
 export const environment = {
-  production: true
+  production: true,
+  token_auth_config: {
+    apiBase:                    'http://localhost:3000',
+    apiPath:                    null,
+
+    signInPath:                 'sign_in',
+    signInRedirect:             null,
+    signInStoredUrlStorageKey:  null,
+
+    signOutPath:                'sign_out',
+    validateTokenPath:          'validate_token',
+    signOutFailedValidate:      false,
+
+    registerAccountPath:        '',
+    deleteAccountPath:          '',
+    registerAccountCallback:    window.location.href,
+
+    updatePasswordPath:         '',
+    resetPasswordPath:          'password',
+    resetPasswordCallback:      window.location.href,
+
+    oAuthBase:                  window.location.origin,
+    oAuthPaths: {
+        github:                 'github'
+    },
+    oAuthCallbackPath:          'oauth_callback',
+    oAuthWindowType:            'newWindow',
+    oAuthWindowOptions:         null,
+
+    userTypes: [
+      { name: 'ADMIN', path: 'admin_auth' },
+      { name: 'USER', path: 'auth' }
+    ],
+
+    globalOptions: {
+      headers: {
+        'Content-Type':     'application/json',
+        'Accept':           'application/json'
+      }
+    },
+  }
 };
