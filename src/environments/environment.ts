ad5b65ffaccbe080922d89f2eb801a2f4789473e
Michael Harris <michaeljh7@gmail.com>
2018-05-12 17:28:52 -0600

    Added feature to scroll to comment edit section if being edited

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,19 @@
 export const environment = {
   production: false,
-  // USERS_API_BASE: 'https://romanway-barber-users.herokuapp.com',
-  // ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/articles',
-  // FEATURED_ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/featured_articles',
-  // COMMENT_USERS_URL: 'https://romanway-barber-news.herokuapp.com/comment_users',
-  // COMMENTS_URL: 'https://romanway-barber-news.herokuapp.com/comments',
-  // TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonials',
-  // FEATURED_TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/featured_testimonials',
-  // TESTIMONIAL_USERS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonial_users'
-  USERS_API_BASE: 'http://localhost:3000',
-  ARTICLES_URL: 'http://localhost:3001/articles',
-  FEATURED_ARTICLES_URL: 'http://localhost:3001/featured_articles',
-  COMMENT_USERS_URL: 'http://localhost:3001/comment_users',
-  COMMENTS_URL: 'http://localhost:3001/comments',
-  TESTIMONIALS_URL: 'http://localhost:3002/testimonials',
-  FEATURED_TESTIMONIALS_URL: 'http://localhost:3002/featured_testimonials',
-  TESTIMONIAL_USERS_URL: 'http://localhost:3002/testimonial_users'
+  USERS_API_BASE: 'https://romanway-barber-users.herokuapp.com',
+  ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/articles',
+  FEATURED_ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/featured_articles',
+  COMMENT_USERS_URL: 'https://romanway-barber-news.herokuapp.com/comment_users',
+  COMMENTS_URL: 'https://romanway-barber-news.herokuapp.com/comments',
+  TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonials',
+  FEATURED_TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/featured_testimonials',
+  TESTIMONIAL_USERS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonial_users'
+  // USERS_API_BASE: 'http://localhost:3000',
+  // ARTICLES_URL: 'http://localhost:3001/articles',
+  // FEATURED_ARTICLES_URL: 'http://localhost:3001/featured_articles',
+  // COMMENT_USERS_URL: 'http://localhost:3001/comment_users',
+  // COMMENTS_URL: 'http://localhost:3001/comments',
+  // TESTIMONIALS_URL: 'http://localhost:3002/testimonials',
+  // FEATURED_TESTIMONIALS_URL: 'http://localhost:3002/featured_testimonials',
+  // TESTIMONIAL_USERS_URL: 'http://localhost:3002/testimonial_users'
 };
