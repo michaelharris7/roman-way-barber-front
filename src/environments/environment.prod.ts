4a26483f962615bae1c91276952958ea970d446f
Michael Harris <michaeljh7@gmail.com>
2018-05-11 19:47:12 -0600

    Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   USERS_API_BASE: 'https://romanway-barber-users.herokuapp.com',
-  ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com',
-  FEATURED_ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com',
-  COMMENT_USERS_URL: 'https://romanway-barber-news.herokuapp.com',
-  COMMENTS_URL: 'https://romanway-barber-news.herokuapp.com',
-  TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com',
-  FEATURED_TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com',
-  TESTIMONIAL_USERS_URL: 'https://romanway-barber-testimonials.herokuapp.com'
+  ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/articles',
+  FEATURED_ARTICLES_URL: 'https://romanway-barber-news.herokuapp.com/featured_articles',
+  COMMENT_USERS_URL: 'https://romanway-barber-news.herokuapp.com/comment_users',
+  COMMENTS_URL: 'https://romanway-barber-news.herokuapp.com/comments',
+  TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonials',
+  FEATURED_TESTIMONIALS_URL: 'https://romanway-barber-testimonials.herokuapp.com/featured_testimonials',
+  TESTIMONIAL_USERS_URL: 'https://romanway-barber-testimonials.herokuapp.com/testimonial_user'
 };
