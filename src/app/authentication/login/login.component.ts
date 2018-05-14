cbfb61d2ab7e8cc57c90d65065a9d26d5388a9be
Michael Harris <michaeljh7@gmail.com>
2018-05-14 16:22:25 -0600

    Fixed observables and included messages for network down on login component

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -21,9 +21,8 @@ export class LoginComponent {
   commentUsers: CommentUser[];
   testimonialUser: TestimonialUser = <TestimonialUser>{};
   testimonialUsers: TestimonialUser[];
-  loadingData: boolean = false;
-  submitted: boolean = false;
-  resetString: string;
+  alertNumber: number = 0;
+  alertMessage: string;
   loginForm: FormGroup;
 
   constructor(
@@ -46,20 +45,45 @@ export class LoginComponent {
 
 
   // General Functions
-  loadingUserAccount() {
+  alertReset() {
     setTimeout(() => {
-      this.resetString = "<p class='alert alert-info mt-4' role='alert'>User account data loading...</p>";
+      this.alertMessage = "";
+      this.loginForm.markAsPristine();
+    });
+    setTimeout(() => {
+      this.alertNumber = 0;
     });
   }
-  loadingUserDataReset() {
+  alertLoading() {
     setTimeout(() => {
-      this.loadingData = false;
-      this.resetString = "";
+      this.alertMessage = "<p class='alert alert-info mt-4' role='alert'>User account data loading...</p>";
+      this.loginForm.markAsPristine();
+    });
+    setTimeout(() => {
+      this.alertNumber = 0;
     });
   }
-  resetSubmit() {
+  alertSubmitted() {
     setTimeout(() => {
-      this.resetString = "<p class='alert alert-primary mt-4' role='alert'>User account logged in successfully. Redirecting now.</p>";
+      this.alertMessage = "<p class='alert alert-primary mt-4' role='alert'>User account logged in successfully. Redirecting now.</p>";
+      this.loginForm.markAsPristine();
+    });
+    setTimeout(() => {
+      this.alertNumber = 0;
+    });
+  }
+  alertNetworkError() {
+    setTimeout(() => {
+      this.alertMessage = "<p class='alert alert-warning mt-4' role='alert'>The user account was not able to be logged into due to a network error. Please contact your network administrator or try again later.</p>";
+      this.loginForm.markAsPristine();
+    });
+    setTimeout(() => {
+      this.alertNumber = 0;
+    });
+  }
+  alertSetToReset() {
+    setTimeout(() => {
+      this.alertNumber = 4;
     });
   }
   redirectToPrevious() {
@@ -71,70 +95,88 @@ export class LoginComponent {
 
   // Login functions
   submit(value: any) {
-    this.loadingData = true;
+    this.alertNumber = 1;
     this.authService.logIn(value.email, value.password)
     .subscribe(
       res => {
         this.userData = this.tokenService.currentUserData;
         this.userType = this.tokenService.currentUserType;
-        this.getCommentUsers();
-        this.getTestimonialUsers();
-        this.redirectToPrevious()
-        this.loadingData = false;
-        this.submitted = true;
+        this.loginAllData();
       },
       err => this.afterFailedUserLogin(value)
     );
   }
   afterFailedUserLogin(value: any) {
-    setTimeout(() => {
-      let formValues = value;
-      console.log(formValues + ', ' + formValues.email);
-      this.authService.logInAdmin(formValues.email, formValues.password)
-      .subscribe(
-        res => {
-          this.userData = this.tokenService.currentUserData;
-          this.userType = this.tokenService.currentUserType;
-          this.getCommentUsers();
-          this.getTestimonialUsers();
-          this.redirectToPrevious();
-          this.loadingData = false;
-          this.submitted = true;
-        },
-        err => this.afterFailedAdminLogin(err)
-      );
-    });
-  }
-  afterFailedAdminLogin(errors: any) {
-    let parsed_errors = JSON.parse(errors._body).errors;
-
-    for(let attribute in this.loginForm.controls) {
-      if (parsed_errors[attribute]) {
-        this.loginForm.controls[attribute]
-            .setErrors(parsed_errors[attribute]);
+    let formValues = value;
+    this.authService.logInAdmin(formValues.email, formValues.password)
+    .subscribe(
+      res => {
+        this.userData = this.tokenService.currentUserData;
+        this.userType = this.tokenService.currentUserType;
+        this.loginAllData();
+      },
+      err => {
+        this.alertNumber = 3;
+        console.log('User account server down. Cannot log user in at this time.');
+        this.authService.logOut();
       }
-    }
-    this.loginForm.setErrors(parsed_errors);
-
-    this.loadingData = false;
+    );
   }
+  // afterFailedAdminLogin(errors: any) {
+  //   this.alertNumber = 3;
+  //   console.log('User account server down. Cannot log user in at this time.');
+  //   this.authService.logOut();
+
+  //   let parsed_errors = JSON.parse(errors._body).errors;
+
+  //   for(let attribute in this.loginForm.controls) {
+  //     if (parsed_errors[attribute]) {
+  //       this.loginForm.controls[attribute]
+  //           .setErrors(parsed_errors[attribute]);
+  //     }
+  //   }
+  //   this.loginForm.setErrors(parsed_errors);
+  // }
 
 
   // CommentUser functions
-  createCommentUserIfNull() {
-    if(this.commentUsers) {
-      if(!this.searchCommentUser(this.userData.id, this.userType)) {
-        let commentUser:CommentUser;
-        this.commentUser.user_id = this.userData.id;
-        this.commentUser.user_type = this.userType;
-        this.commentUser.user_name = this.userData.name;
-        this.articleService.createCommentUser(this.commentUser).subscribe(
-          res => console.log('Comment User created successfully'),
-          err => console.log(err)
-          );
-      } else {
-        console.log('Comment user already exists');
+  loginAllData() {  // getCommentUsers() first
+    this.articleService.getCommentUsers()
+    .subscribe(
+      commentUsers => {
+        console.log('Article server found');
+        this.commentUsers = commentUsers;
+
+        if(this.commentUsers)
+          this.createCommentUserIfNull();
+      },
+      err => {
+        this.alertNumber = 3;
+        console.log('Article server down. Cannot load comment user at this time.');
+        this.authService.logOut();
       }
+    );
+  }
+  createCommentUserIfNull() {
+    if(!this.searchCommentUser(this.userData.id, this.userType)) {
+      let commentUser:CommentUser;
+      this.commentUser.user_id = this.userData.id;
+      this.commentUser.user_type = this.userType;
+      this.commentUser.user_name = this.userData.name;
+      this.articleService.createCommentUser(this.commentUser).subscribe(
+        res => {
+          console.log('Comment User created successfully');
+          this.getTestimonialUsers();
+        },
+        err => {
+          this.alertNumber = 3;
+          console.log('There was an error creating the comment user: ' + err);
+          this.authService.logOut();
+        }
+        );
+    } else {
+      console.log('Comment user already exists');
+      this.getTestimonialUsers();
     }
   }
   searchCommentUser(user_id:number, user_type:string):boolean {
@@ -145,46 +187,9 @@ export class LoginComponent {
     }
     return match;
   }
-  getCommentUsers() {
-    this.articleService.getCommentUsers()
-    .subscribe(
-      commentUsers => {
-        console.log('Comment user server found');
-        this.commentUsers = commentUsers;
-
-        if(this.commentUsers)
-          this.createCommentUserIfNull();
-      },
-      err => console.log('Article server down. Cannot create comment user at this time.')
-    );
-  }
 
 
   // TestimonialUser functions
-  createTestimonialUserIfNull() {
-    if(this.testimonialUsers) {
-      if(!this.searchTestimonialUser(this.userData.id, this.userType)) {
-        let testimonialUser:TestimonialUser;
-        this.testimonialUser.user_id = this.userData.id;
-        this.testimonialUser.user_type = this.userType;
-        this.testimonialUser.user_name = this.userData.name;
-        this.testimonialService.createTestimonialUser(this.testimonialUser).subscribe(
-          res => console.log('Testimonial User created successfully'),
-          err => console.log(err)
-          );
-      } else {
-        console.log('Testimonial user already exists');
-      }
-    }
-  }
-  searchTestimonialUser(user_id:number, user_type:string):boolean {
-    let match:boolean = false;
-    for(let testimonialUser of this.testimonialUsers) {
-      if((testimonialUser.user_id === user_id) && (testimonialUser.user_type === user_type))
-        match = true;
-    }
-    return match;
-  }
   getTestimonialUsers() {
     this.testimonialService.getTestimonialUsers()
     .subscribe(
@@ -195,7 +200,43 @@ export class LoginComponent {
         if(this.testimonialUsers)
           this.createTestimonialUserIfNull();
       },
-      err => console.log('Testimonial server down. Cannot create comment user at this time.')
+      err => {
+        this.alertNumber = 3;
+        console.log('Testimonial server down. Cannot load testimonial user at this time.');
+        this.authService.logOut();
+      }
     );
+  }
+  createTestimonialUserIfNull() {
+    if(!this.searchTestimonialUser(this.userData.id, this.userType)) {
+      let testimonialUser:TestimonialUser;
+      this.testimonialUser.user_id = this.userData.id;
+      this.testimonialUser.user_type = this.userType;
+      this.testimonialUser.user_name = this.userData.name;
+      this.testimonialService.createTestimonialUser(this.testimonialUser).subscribe(
+        res => {
+          console.log('Testimonial User created successfully');
+          this.redirectToPrevious(); // Data is loaded successfully
+          this.alertNumber = 2;
+        },
+        err => {
+          this.alertNumber = 3;
+          console.log('There was an error creating the testimonial user: ' + err);
+          this.authService.logOut();
+        }
+      );
+    } else {
+      console.log('Testimonial user already exists');
+      this.redirectToPrevious(); // Data is loaded successfully
+      this.alertNumber = 2;
+    }
+  }
+  searchTestimonialUser(user_id:number, user_type:string):boolean {
+    let match:boolean = false;
+    for(let testimonialUser of this.testimonialUsers) {
+      if((testimonialUser.user_id === user_id) && (testimonialUser.user_type === user_type))
+        match = true;
+    }
+    return match;
   }
 }
