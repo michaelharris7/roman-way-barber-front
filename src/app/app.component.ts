d933da022897174f8230b634ce56a4a6feeb9ef7
Michael Harris <michaeljh7@gmail.com>
2018-05-24 15:05:06 -0600

    created redirection to https on account pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { MarkdownService } from 'angular2-markdown';
 
 
@@ -10,13 +11,17 @@ import { MarkdownService } from 'angular2-markdown';
 export class AppComponent implements OnInit {
 
   constructor(
+    private router: Router,
     private markdown: MarkdownService
-  ) {}
+  ) {router.events.subscribe(() => {
+      this.redirectOnAccount();
+    });
+  }
 
   ngOnInit() {
     this.markDownSetUp();
+    console.log(document.location.hostname);
   }
-
 
   markDownSetUp() {
     this.markdown.renderer.table = (header: string, body: string) => {
@@ -34,5 +39,33 @@ export class AppComponent implements OnInit {
     this.markdown.renderer.blockquote = (quote: string) => {
       return `<blockquote class="markdown-quote">${quote}</blockquote>`;
     }
+  }
+  isAccountPage() {
+    switch (document.location.pathname)
+    {
+    case'/register':
+    case'/login':
+    case'/forgot-password':
+    case'/account':
+      return true;
+    default:
+      return false;
+    }
+  }
+  redirectOnAccount() {
+    console.log("redirect if on account: " + document.location.href);
+    console.log("document.location.protocol: " + document.location.protocol);
+    if (this.isAccountPage()) {
+      if (document.location.protocol === 'http:') {
+        document.location.href = 'https://evansbarberstylist.herokuapp.com' + document.location.pathname;
+        console.log("redirecting in process... " + document.location.href);
+      }
+    } else {
+      if (document.location.hostname !== 'evansbarberstylist.com') {
+        document.location.href = 'http://evansbarberstylist.com' + document.location.pathname;
+        console.log("redirecting in process... " + document.location.href);
+      }
+    }
+    console.log("redirect occured: " + document.location.href);
   }
 }
